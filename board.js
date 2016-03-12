300e0c1638868782bd4545d3f6d7b5cd37c282b6
Hendrixer <willscottmoss@gmail.com>
2016-03-11 23:40:56 -0800

    chore

--- board.js
+++ board.js
@@ -8,7 +8,7 @@ var board = new five.Board({
 board.on('ready', function() {
   console.log('board ready');
   var button = new five.Button({
-    pin: 'GPIO24',
+    pin: 5,
     isPullup: true
   });
 
