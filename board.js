8527469a8cbb745500e91a4119fc9071af42545d
Hendrixer <willscottmoss@gmail.com>
2016-03-11 23:34:18 -0800

    chore

--- board.js
+++ board.js
@@ -8,7 +8,7 @@ var board = new five.Board({
 board.on('ready', function() {
   console.log('board ready');
   var button = new five.Button({
-    pin: 'GPIO18',
+    pin: 'GPIO24',
     holdtime: 1500
   });
 
